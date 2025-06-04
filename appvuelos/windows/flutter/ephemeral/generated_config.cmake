61c0189992d5b24983e711dff56e18092dad8b95
mija450 <gatocr2005@gmail.com>
2025-06-04 11:12:53 -0500

    Add files via upload

---
+++ appvuelos/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\gatoc\\Downloads\\flutter_windows_3.27.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\xampp\\htdocs\\ProyectoVuelos\\appvuelos" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\gatoc\\Downloads\\flutter_windows_3.27.3-stable\\flutter"
+  "PROJECT_DIR=C:\\xampp\\htdocs\\ProyectoVuelos\\appvuelos"
+  "FLUTTER_ROOT=C:\\Users\\gatoc\\Downloads\\flutter_windows_3.27.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\xampp\\htdocs\\ProyectoVuelos\\appvuelos\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\xampp\\htdocs\\ProyectoVuelos\\appvuelos"
+  "FLUTTER_TARGET=C:\\xampp\\htdocs\\ProyectoVuelos\\appvuelos\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\xampp\\htdocs\\ProyectoVuelos\\appvuelos\\.dart_tool\\package_config.json"
+)
